4f3962b4c97dcce5e1dd7b13d33d94a67b41330a
riviere j seguie <rjs76@osl12.njit.edu>
2018-04-25 19:46:18 -0400

    MVC2

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
     $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $username = 'rjs76';
+    $password = 'Dualshock77';
 
     try {
         $db = new PDO($dsn, $username, $password);
